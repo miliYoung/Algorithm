9cdd6af5c42ae99160d94c36274b910b1c66d5dd
Mili Young <miliyang1224@163.com>
2018-05-15 07:55:59 +0800

    commit traverse

---
+++ traverse.h
@@ -0,0 +1,11 @@
+#ifndef _TRAVERSE_H
+#define _TRAVERSE_H
+
+
+int preorder(const BiTreeNode* node, List* list);
+
+int inorder(const BiTreeNode* node, List* list);
+
+int postorder(const BiTreeNode* node, List* list);
+
+#endif
