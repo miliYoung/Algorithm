3414042050b5af9518abbcabb6c022b26ac08174
Mili Young <miliyang1224@163.com>
2018-05-18 00:32:03 +0800

    add dlist

---
+++ dlist.c
@@ -0,0 +1,28 @@
+#include "dlist.h"
+
+void dlist_init(DList* dlist, void (*destory)(void* data))
+{
+
+
+}
+
+
+void dlist_destroy(DList* list)
+{
+
+}
+
+int dlist_ins_next(DList* list, DListElmt* element, const void* data)
+{
+
+}
+
+int dlist_ins_prev(DList* list, DListElmt* element, const void* data)
+{
+
+}
+
+int dlist_remove(DList* list,DListElmt* element, void** data)
+{
+
+}
