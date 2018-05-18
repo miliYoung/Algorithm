0e9c4045a2384d9431fa379b2f14ae399a2676cf
Mili Young <miliyang1224@163.com>
2018-05-18 23:12:32 +0800

    add stack

--- stack.h
+++ stack.h
@@ -1,6 +1,21 @@
 #ifndef _STACK_H
 #define _STACK_H
 
+#include <stdlib.h>
+#include "list.h"
 
+typedef list stack;
+
+#define stack_init list_init
+
+#define stack_destroy list_destroy
+
+#define stack_size list_size
+
+#define stack_peek(stack) (stack)->head == NULL? NULL:(stack)->head->data
+
+int stack_push(Stack* stack, void* data);
+
+int stack_pop(Stack* stack, void** data);
 
 #endif
