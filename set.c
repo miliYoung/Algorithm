50ff27bd2766b8c23ca1a8497518659641efec97
miliYoung <miliyang1224@163.com>
2018-05-19 09:17:38 +0800

    Create set.c

---
+++ set.c
@@ -0,0 +1,46 @@
+#include "set.h"
+
+void set_init(Set* set, int (*match)(void* key1, void* key2), void (*destroy)(void* data))
+{
+
+}
+
+int set_insert(Set* set, const void* data)
+{
+
+}
+
+int set_remove(Set* set, void** data)
+{
+  
+}
+
+int set_union(Set* setu, const Set *set1, const Set* set2)
+{
+  
+}
+
+int set_intersection(Set* seti,const Set* set1, const Set* set2)
+{
+  
+}
+
+int set_difference(Set* setd, const Set* set1, const Set* set2)
+{
+  
+}
+
+int set_is_member(const Set* set, const void* data)
+{
+  
+}
+
+int sub_is_subset(const Set* set1, const Set* set2)
+{
+
+}
+
+int set_is_equal(const Set* set1, const Set* set2)
+{
+
+}
